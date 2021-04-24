f0f68c4b2fb245ea1a32984d34b655ff4d855b8f
Loc Do <lockunlatui@gmail.com>
2021-04-24 15:23:48 +0700

    apply ui

---
+++ index.js
@@ -0,0 +1,25 @@
+// const imagesSlider = [
+// 	{
+// 		id: `1`,
+// 		img: `https://photo-zmp3.zadn.vn/banner/e/b/3/3/eb33819eecbca631459733e42b797714.jpg`,
+// 	},
+// 	{
+// 		id: `2`,
+// 		img: `https://photo-zmp3.zadn.vn/banner/6/c/6/6/6c66e39664351e709716d09fbcb3e693.jpg`,
+// 	},
+// 	{
+// 		id: `3`,
+// 		img: `https://photo-zmp3.zadn.vn/banner/2/b/a/c/2bac015590e4e9abaeac419877b10cc4.jpg`,
+// 	},
+// 	{
+// 		id: `4`,
+// 		img: `https://photo-zmp3.zadn.vn/banner/f/f/6/b/ff6bc6f48a83f6025024e7e49aa03348.jpg`,
+// 	},
+// 	{
+// 		id: `5`,
+// 		img: `https://photo-zmp3.zadn.vn/banner/f/0/0/0/f0003946de8852677c4f3b1187006333.jpg`,
+// 	},
+// ];
+
+const galleryItem = document.querySelectorAll('.gallery-item');
+console.log('galleryItem', galleryItem)
